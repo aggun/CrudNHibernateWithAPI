9f019fe3956f1ff73e78a04a8b2b8a8a591745ec
Nebil Aggun <nebil.aggun@gmail.com>
2022-08-21 17:44:31 +0300

    Middleware created

---
+++ NHibernateCRUD/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace NHibernateCRUD.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                var messageError = string.Empty;
+
+
+                messageError = "Internal Server Error";
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                // file logging 
+
+                var result = JsonSerializer.Serialize(messageError);
+                await response.WriteAsync(result);
+            }
+        }
+
+    }
+}
